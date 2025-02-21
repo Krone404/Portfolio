4a3e0a38fa1d0731bb78c0b0c65e864f660590a8
Cameron <cameron.cartwright122804@gmail.com>
2025-02-21 01:26:38 +0000

    CSS start

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#629460', // Asparagus
+        secondary: '#96be8c', // Olivine
+        accent: '#aceca1', // Light Green
+        background: '#c9f2c7', // Tea Green
+        dark: '#243119', // Dark Green
+      },
+    },
   },
-  plugins: [],
-}
+};
 
