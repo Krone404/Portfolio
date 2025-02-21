29487c7cc50faedb46b1d3c3f09d3caceffe5c3c
Cameron <cameron.cartwright122804@gmail.com>
2025-02-21 09:46:54 +0000

    Update next.config.mjs

--- my-portfolio/next.config.mjs
+++ my-portfolio/next.config.mjs
@@ -1,12 +1,20 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     images: {
       unoptimized: true,
     },
     basePath: '/Portfolio', // Replace with your repo name
-    assetPrefix: '/Portfolio/', // Ensures assets load correctly
+    assetPrefix: '/Portfolio/',
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        ...defaultPathMap,
+        '/.nojekyll': { page: '/' }, // Include the .nojekyll file in the export
+      };
+    },
   };
   
-  export default nextConfig;
+  module.exports = nextConfig;
   
